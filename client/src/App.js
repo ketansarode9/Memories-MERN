80309950f3a31e80b249c1350f36c40b364fbf94
ketansarode9 <ketan.sarode47@gmail.com>
2022-02-11 23:17:53 +0530

    changes in APP.JS

--- client/src/App.js
+++ client/src/App.js
@@ -28,11 +28,9 @@ const App = () => {
           <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
               <Posts setCurrentId={setCurrentId} /> 
-              {/* setCurrentId={setCurrentId} */}
             </Grid>
             <Grid item xs={12} sm={4}>
               <Form  currentId={currentId} setCurrentId={setCurrentId}/> 
-              {/* currentId={currentId} setCurrentId={setCurrentId} */}
             </Grid>
           </Grid>
         </Container>
