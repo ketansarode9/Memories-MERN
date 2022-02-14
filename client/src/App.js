0bbea0220bc8970ae59bba758770a107a96ee72c
ketansarode9 <ketan.sarode47@gmail.com>
2022-02-14 23:28:45 +0530

    added files for auth - sign-in/up

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
 
-import Posts from './components/Posts/Posts.js';
-import Form from './components/Form/Form.js';
-import { getPosts } from './actions/posts';
-import useStyles from './styles';
-import memories from './images/memories.png';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const App = () => {
-  const [currentId, setCurrentId] = useState(0);
-  const dispatch = useDispatch();
-   const classes = useStyles();
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Auth from './components/Auth/Auth';
 
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
-  return (
+const App = () => (
+  <BrowserRouter>
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading}  variant="h2" align="center">Memories</Typography>
-        <img  className={classes.image} src={memories} alt="icon" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} /> 
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form  currentId={currentId} setCurrentId={setCurrentId}/> 
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Routes>
     </Container>
-  );
-};
+  </BrowserRouter>
+);
 
 export default App;
